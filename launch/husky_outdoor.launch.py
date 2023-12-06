a23934619a56b896b295f9435f689528f1afd8df
aaqibsb <aaqib.barodawala@gmail.com>
2023-12-05 20:45:33 -0500

    Task 1.2.11 Fixed Launch

---
+++ launch/husky_outdoor.launch.py
@@ -0,0 +1,39 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    
+    world_file = PathJoinSubstitution(
+        [FindPackageShare("husky_gazebo"),
+        "worlds",
+        "outdoor.world"]
+    )
+
+    gazebo_launch = PathJoinSubstitution(
+        [FindPackageShare("husky_gazebo"),
+        "launch",
+        "gazebo.launch.py"],
+    )
+
+    gazebo_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([gazebo_launch]),
+        launch_arguments={'world_path': world_file}.items(),
+    )
+    
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(gazebo_sim)
+    ld.add_action(rviz_node)
+
+    return ld
