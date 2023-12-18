49599e31481dddcf8ed000f30e5d1bdb21ec8c66
VEDANT RANADE <vedantmranade@gmail.com>
2023-12-18 18:41:53 -0500

    Task 2.2.1 Updated the launch file to inlcude rviz config

--- launch/husky_outdoor.launch.py
+++ launch/husky_outdoor.launch.py
@@ -7,7 +7,7 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    
+
     world_file = PathJoinSubstitution(
         [FindPackageShare("spectral_fubar"),
         "worlds",
@@ -20,16 +20,23 @@ def generate_launch_description():
         "gazebo.launch.py"],
     )
 
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("spectral_fubar"),
+        "rviz",
+        "default.rviz"]
+    )
+
     gazebo_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([gazebo_launch]),
         launch_arguments={'world_path': world_file}.items(),
     )
-    
+
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         output='screen',
+        arguments=['-d', rviz_config_file]
     )
 
     ld = LaunchDescription()
