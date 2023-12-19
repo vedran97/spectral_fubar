b484f4c84a8b9aeab976bf15d6d689aa5ed1056b
VEDANT RANADE <vedantmranade@gmail.com>
2023-12-19 16:10:13 -0500

    Added retrieval of model states in inspector lib
    Get model state in inspector

--- include/spectral_fubar/HuskyInspector.hpp
+++ include/spectral_fubar/HuskyInspector.hpp
@@ -6,6 +6,7 @@
 #include <rclcpp/logging.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
+#include <gazebo_msgs/msg/model_states.hpp>
 namespace husky {
 class Inspector : public rclcpp::Node {
  public:
@@ -16,6 +17,9 @@ class Inspector : public rclcpp::Node {
   geometry_msgs::msg::Twist cmdVel_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr commandVelPublisher_;
   rclcpp::TimerBase::SharedPtr cmdVelTimer_;
+   rclcpp::Subscription<gazebo_msgs::msg::ModelStates>::SharedPtr subscription_;
+   void modelStatesCallback(const gazebo_msgs::msg::ModelStates::SharedPtr msg);
+   geometry_msgs::msg::Pose robotPose_;
 };
 };  // namespace husky
 #endif
--- libs/HuskyInspector.cpp
+++ libs/HuskyInspector.cpp
@@ -9,8 +9,21 @@ Inspector::Inspector() : Node("inspector") {
   cmdVelTimer_ =
       this->create_wall_timer(std::chrono::milliseconds(100),
                               std::bind(&Inspector::cmdVelPublisher, this));
+subscription_ = create_subscription<gazebo_msgs::msg::ModelStates>(
+            "/gazebo/model_states", 10,
+            std::bind(&Inspector::modelStatesCallback, this, std::placeholders::_1));
 }
 void Inspector::cmdVelPublisher(){
     this->commandVelPublisher_->publish(cmdVel_);
 }
+  void Inspector::modelStatesCallback(const gazebo_msgs::msg::ModelStates::SharedPtr msg) {
+    for (size_t i = 0; i < msg->name.size(); ++i) {
+      if (msg->name[i] == "husky") {
+         robotPose_ = msg->pose[i];
+        RCLCPP_INFO(get_logger(), "Husky Pose: x=%f, y=%f, z=%f",
+                    robotPose_.position.x, robotPose_.position.y, robotPose_.position.z);
+        break;  // Exit loop once "husky" is found
+      }
+    }
+  }
 }  // namespace husky
