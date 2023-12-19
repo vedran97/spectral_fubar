643934af22fc0462bf80a261f09d55b041041117
VEDANT RANADE <vedantmranade@gmail.com>
2023-12-19 17:33:42 -0500

    Added basic motion functions

--- include/spectral_fubar/HuskyInspector.hpp
+++ include/spectral_fubar/HuskyInspector.hpp
@@ -1,26 +1,36 @@
 #ifndef HUSKY_INSPECTOR_HPP
 #define HUSKY_INSPECTOR_HPP
+#include <gazebo_msgs/msg/model_states.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <mutex>
 #include <rclcpp/logger.hpp>
 #include <rclcpp/logging.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
-#include <gazebo_msgs/msg/model_states.hpp>
 namespace husky {
 class Inspector : public rclcpp::Node {
  public:
   Inspector();
 
  private:
-  void cmdVelPublisher();
+  geometry_msgs::msg::Pose robotPose_;
   geometry_msgs::msg::Twist cmdVel_;
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr commandVelPublisher_;
   rclcpp::TimerBase::SharedPtr cmdVelTimer_;
-   rclcpp::Subscription<gazebo_msgs::msg::ModelStates>::SharedPtr subscription_;
-   void modelStatesCallback(const gazebo_msgs::msg::ModelStates::SharedPtr msg);
-   geometry_msgs::msg::Pose robotPose_;
-    void quaternionToRPY(const geometry_msgs::msg::Quaternion& quaternion, double& roll, double& pitch, double& yaw);
+  void cmdVelPublisher();
+
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr commandVelPublisher_;
+
+  rclcpp::Subscription<gazebo_msgs::msg::ModelStates>::SharedPtr subscription_;
+  void modelStatesCallback(const gazebo_msgs::msg::ModelStates::SharedPtr msg);
+
+  void quaternionToRPY(const geometry_msgs::msg::Quaternion& quaternion,
+                       double& roll, double& pitch, double& yaw);
+  // @brief: Publishes on cmd_vel topic to move the robot forward in x direction
+  inline void forward();
+  // @brief: Publishes on cmd_vel topic to turn the robot in z direction
+  inline void turn(int right);
+  // @brief: Publishes on cmd_vel topic to stop the robot
+  inline void stop();
 };
 };  // namespace husky
 #endif
--- libs/HuskyInspector.cpp
+++ libs/HuskyInspector.cpp
@@ -1,5 +1,7 @@
 #include "HuskyInspector.hpp"
+
 #include <tf2/LinearMath/Quaternion.h>
+
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 namespace husky {
 Inspector::Inspector() : Node("inspector") {
@@ -10,26 +12,51 @@ Inspector::Inspector() : Node("inspector") {
   cmdVelTimer_ =
       this->create_wall_timer(std::chrono::milliseconds(100),
                               std::bind(&Inspector::cmdVelPublisher, this));
-subscription_ = create_subscription<gazebo_msgs::msg::ModelStates>(
-            "/gazebo/model_states", 10,
-            std::bind(&Inspector::modelStatesCallback, this, std::placeholders::_1));
+  subscription_ = create_subscription<gazebo_msgs::msg::ModelStates>(
+      "/gazebo/model_states", 10,
+      std::bind(&Inspector::modelStatesCallback, this, std::placeholders::_1));
 }
-void Inspector::cmdVelPublisher(){
-    this->commandVelPublisher_->publish(cmdVel_);
+void Inspector::cmdVelPublisher() {
+  this->commandVelPublisher_->publish(cmdVel_);
 }
-  void Inspector::modelStatesCallback(const gazebo_msgs::msg::ModelStates::SharedPtr msg) {
-    for (size_t i = 0; i < msg->name.size(); ++i) {
-      if (msg->name[i] == "husky") {
-         robotPose_ = msg->pose[i];
-        RCLCPP_DEBUG(get_logger(), "Husky Pose: x=%f, y=%f, z=%f",
-                    robotPose_.position.x, robotPose_.position.y, robotPose_.position.z);
-        break;  // Exit loop once "husky" is found
-      }
+void Inspector::modelStatesCallback(
+    const gazebo_msgs::msg::ModelStates::SharedPtr msg) {
+  for (size_t i = 0; i < msg->name.size(); ++i) {
+    if (msg->name[i] == "husky") {
+      robotPose_ = msg->pose[i];
+      RCLCPP_DEBUG(get_logger(), "Husky Pose: x=%f, y=%f, z=%f",
+                   robotPose_.position.x, robotPose_.position.y,
+                   robotPose_.position.z);
+      break;  // Exit loop once "husky" is found
     }
   }
-  void Inspector::quaternionToRPY(const geometry_msgs::msg::Quaternion& quaternion, double& roll, double& pitch, double& yaw) {
-    tf2::Quaternion tf_quaternion(quaternion.x, quaternion.y, quaternion.z, quaternion.w);
-    tf2::Matrix3x3(tf_quaternion).getRPY(roll, pitch, yaw);
+}
+void Inspector::quaternionToRPY(
+    const geometry_msgs::msg::Quaternion& quaternion, double& roll,
+    double& pitch, double& yaw) {
+  tf2::Quaternion tf_quaternion(quaternion.x, quaternion.y, quaternion.z,
+                                quaternion.w);
+  tf2::Matrix3x3(tf_quaternion).getRPY(roll, pitch, yaw);
+}
+
+// @brief: Publishes on cmd_vel topic to move the robot forward in x direction
+inline void Inspector::forward() {
+  RCLCPP_INFO_STREAM(this->get_logger(), "Moving forward");
+
+  this->cmdVel_ = geometry_msgs::msg::Twist();
+  this->cmdVel_.linear.x = 0.5;
+}
+// @brief: Publishes on cmd_vel topic to turn the robot in z direction
+inline void Inspector::turn(int right) {
+  RCLCPP_INFO_STREAM(this->get_logger(), "Turning");
+  this->cmdVel_ = geometry_msgs::msg::Twist();
+  cmdVel_.angular.z = (right - 1) * 0.2;
+  commandVelPublisher_->publish(cmdVel_);
+}
+// @brief: Publishes on cmd_vel topic to stop the robot
+inline void Inspector::stop() {
+  RCLCPP_INFO_STREAM(this->get_logger(), "Stopping");
+  this->cmdVel_ = geometry_msgs::msg::Twist();
 }
 
 }  // namespace husky
