b8f04e3b26e90d3edb3f7f1510574584845b1a3d
VEDANT RANADE <vedantmranade@gmail.com>
2023-12-19 16:41:40 -0500

    Initialized the inspector lib executable

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,7 @@
 #include "HuskyInspector.hpp"
-int main() { return 0; }
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<husky::Inspector>());
+  rclcpp::shutdown();
+  return 0;
+}
