ac83d3de7432ee37baaa74eda1729c152c34a5a9
VEDANT RANADE <vedantmranade@gmail.com>
2023-12-19 14:12:21 -0500

    2.2.1.1 Started implementing HuskyInspector

---
+++ include/spectral_fubar/HuskyInspector.hpp
@@ -0,0 +1,21 @@
+#ifndef HUSKY_INSPECTOR_HPP
+#define HUSKY_INSPECTOR_HPP
+#include <geometry_msgs/msg/twist.hpp>
+#include <mutex>
+#include <rclcpp/logger.hpp>
+#include <rclcpp/logging.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+namespace husky{
+class Inspector{
+    public:
+    Inspector(){
+        ;
+    }
+    private:
+    void cmdVelPublisher();
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr commandVelPublisher_;
+    rclcpp::TimerBase::SharedPtr cmdVelTimer;
+};
+};
+#endif
